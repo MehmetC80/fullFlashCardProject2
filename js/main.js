bc88674f605c2038ccf3aecea04532a958f9b205
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-03-14 14:07:26 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-import { test } from "./tools.js";
+import * as tools from "./tools.js";
+import { flashcards } from "./data/flashcards.js";
 
 const contentElem = document.querySelector(".content");
 
-contentElem.innerHTML = "test";
+contentElem.innerHTML = `
+<p>${tools.getTodaysDate()}</p>
+<div class="flashcards">
+	${flashcards
+    .map((flashcard) => {
+      return `
+	<div class="flashcard">
+		<div class="front"><span class="category">${flashcard.category.toUpperCase()}</span>: ${tools.capitalizeFirstCharacter(
+        flashcard.front
+      )}</div>
+		<div class="back">${flashcard.back}</div>	
+	</div>	
+		`;
+    })
+    .join("")}
+</div>
+`;
